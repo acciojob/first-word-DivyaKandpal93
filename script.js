6bf30ce4d584bd62307ba4b8a836e6d45942ce26
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2024-02-19 23:31:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 function firstWord(sentence) {
-  // your code here
-	 try {
-        if (sentence.trim() === '') {
-            return sentence;
-        } else if (!sentence.includes(' ')) {
-            return sentence;
-        } else {
-            const words = sentence.split(' ');
-            return words[0];
-        }
-    } catch (error) {
-        console.error("An error occurred:", error.message);
-        // You can choose to return a default value or handle the error in any appropriate manner
-        return null;
+    const trimmedSentence = sentence.trim();
+    if (trimmedSentence === '' || !trimmedSentence.includes(' ')) {
+        return trimmedSentence;
+    } else {
+        const words = trimmedSentence.split(' ');
+        return words[0];
     }
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
